67beb27ec519405c11d0167f597758cd0a65b4e2
asharaf <asharafansari485@gmail.com>
2025-09-13 09:56:58 +0530

    initial learning codes

--- langgraph_code/initializeLLM.py
+++ langgraph_code/initializeLLM.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
+from langchain.chat_models import init_chat_model 
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -7,12 +8,24 @@ def get_openai_llm():
     llm = init_chat_model("gpt-4o-mini", model_provider="openai")
     return llm
 
-def get_google_genai_llm():
-    pass
+def get_ollama_llm():
+    llm = ChatOllama(
+        base_url="http://localhost:11434", 
+        model="llama3.2:latest"
+    )
+    return llm
 
-
+def get_ollama_embedding():
+    from langchain_ollama import OllamaEmbeddings
+    embedding = OllamaEmbeddings(
+        base_url="http://localhost:11434", 
+        model="nomic-embed-text:latest"
+    )
+    return embedding
 
 if __name__ == "__main__":
-    model = get_openai_llm()
-    response = model.invoke("Hello, How are you?")
-    print(response.content)
+    # model = get_openai_llm()
+    # print(get_ollama_llm())
+    get_ollama_embedding()
+    # response = model.invoke("Hello, How are you?")
+    # print(response.content)
